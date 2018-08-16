7712b97b2a670d478169c9393e31740961c71c5e
tanuha <tanuha@zeoalliance.com>
2018-08-16 16:45:11 +0300

    done 5 tests

--- public/js/ImagesGallery.js
+++ public/js/ImagesGallery.js
@@ -1,74 +1,100 @@
 window.ImageGallery = (function () {
-  class ImageGallery {
-    /**
-     * @constructor
-     * @param {ImagesResolver} imagesResolver
-     */
-    constructor(imagesResolver) {
-      this.imagesResolver = imagesResolver;
-      this._initView();
-      this._initViewFunctionality();
+    const availableDB = ['local', 'pixabay'];
+
+    class ImageGallery {
+        /**
+         * @constructor
+         * @param {ImagesResolver} imagesResolver
+         */
+        constructor(imagesResolver) {
+            this.imagesResolver = imagesResolver;
+            this._initView();
+            this._initViewFunctionality();
+            this.query = null;
+        }
+
+        /**
+         * @param {String} query
+         */
+        search(query, searchModuleId) {
+            if (!availableDB.includes(searchModuleId)) {
+                throw new Error('There is no db');
+            }
+
+            this.query = query;
+
+            this.imagesResolver.search(query, searchModuleId)
+                .then((searchResult) => {
+                    if (this.query === query) {
+                        this._onReceiveSearchResult(searchResult);
+                    }
+                });
+        }
+
+        addToElement(element) {
+            element.appendChild(this.container);
+        }
+
+        _onUserSearch(ev) {
+            ev.preventDefault();
+            this.search(this.seachInput.value, this.selectDb.value);
+        }
+
+        _onReceiveSearchResult(result) {
+            this.searchResults.innerHTML = "";
+            const imagesInfo = result.images;
+
+            imagesInfo.forEach((image) => {
+                const imgNode = document.createElement('img');
+                imgNode.setAttribute('src', image.url);
+                this.searchResults.appendChild(imgNode);
+            });
+        }
+
+        _initView() {
+            this.container = document.createElement("div");
+            this.container.className = "gallery";
+
+            this.form = document.createElement("form");
+            this.form.className = "gallery__form form-inline";
+            this.container.appendChild(this.form);
+
+            this.formGroup = document.createElement("div");
+            this.formGroup.className = "form-group";
+            this.form.appendChild(this.formGroup);
+
+            this.seachInput = document.createElement("input");
+            this.seachInput.className = "gallery__search form-control";
+            this.seachInput.placeholder = "search by tag";
+            this.formGroup.appendChild(this.seachInput);
+
+            this.optionLocal = document.createElement('option');
+
+            this.optionLocal.text = 'local';
+            this.optionPixabay = document.createElement('option');
+            this.optionPixabay.text = 'pixabay';
+
+            this.selectDb = document.createElement('select');
+            this.selectDb.className = "gallery__select";
+            this.selectDb.add(this.optionLocal);
+            this.selectDb.add(this.optionPixabay);
+            this.form.appendChild(this.selectDb);
+
+            this.searchButton = document.createElement("button");
+            this.searchButton.className = "gallery__button btn btn-primary";
+            this.searchButton.innerText = "search";
+            this.form.appendChild(this.searchButton);
+
+            this.searchResults = document.createElement("div");
+            this.searchResults.className = "gallery__result";
+            this.container.appendChild(this.searchResults);
+        }
+
+        _initViewFunctionality() {
+            this.form.addEventListener("submit", this._onUserSearch.bind(this));
+            this.seachInput.addEventListener('keyup', this._onUserSearch.bind(this));
+        }
     }
 
-    /**
-     * @param {String} query
-     */
-    search(query) {
-      const searchResults = this.imagesResolver.search(query);
-      this._onReceiveSearchResult(searchResults);
-    }
-
-    addToElement(element) {
-      element.appendChild(this.container);
-    }
-
-    _onUserSearch(ev) {
-      ev.preventDefault();
-      this.search(this.seachInput.value);
-    }
-
-    _onReceiveSearchResult(result) {
-      this.searchResults.innerHTML = "";
-      const imagesInfo = result.images;
-
-      imagesInfo.forEach((image) => {
-        const imgNode = document.createElement('img');
-        imgNode.setAttribute('src', image.url);
-        this.searchResults.appendChild(imgNode);
-      });
-    }
-
-    _initView() {
-      this.container = document.createElement("div");
-      this.container.className = "gallery";
-
-      this.form = document.createElement("form");
-      this.form.className = "gallery__form form-inline";
-      this.container.appendChild(this.form);
-
-      this.formGroup = document.createElement("div");
-      this.formGroup.className = "form-group";
-      this.form.appendChild(this.formGroup);
-
-      this.seachInput = document.createElement("input");
-      this.seachInput.className = "gallery__search form-control";
-      this.seachInput.placeholder = "search by tag";
-      this.formGroup.appendChild(this.seachInput);
-
-      this.searchButton = document.createElement("button");
-      this.searchButton.className = "gallery__button btn btn-primary";
-      this.searchButton.innerText = "search";
-      this.form.appendChild(this.searchButton);
-
-      this.searchResults = document.createElement("div");
-      this.searchResults.className = "gallery__result";
-      this.container.appendChild(this.searchResults);
-    }
-
-    _initViewFunctionality() {
-      this.form.addEventListener("submit", this._onUserSearch.bind(this));
-    }
-  }
-
-  return ImageGallery;
+    return ImageGallery;
 })();
--- public/js/ImagesResolver.js
+++ public/js/ImagesResolver.js
@@ -1,27 +1,60 @@
 window.ImagesResolver = (function () {
-  class ImagesResolver {
-    constructor() {
+    class ImagesResolver {
+        constructor() {
+            this.query = {
+                'local': {},
+                'pixabay': {}
+            };
+        }
 
+        reformatDb(db) {
+            return db.map((item) => {
+                    return {
+                        id: item.id,
+                        url: item.previewURL,
+                        tags: item.tags
+                    }
+                });
+        }
+
+        localSearch(query) {
+            const localDb = window.localDB.filter((item) => {
+                return item.tags.split(/, /g).includes(query);
+            });
+
+            const result =  {
+                query: query,
+                images: this.reformatDb(localDb)
+            };
+
+            return Promise.resolve(result);
+        }
+
+        pixabaySearch(query) {
+            return fetch('https://pixabay.com/api/?key=8522875-59a2673910903be627161f155&q=' + query + '&image_type=all&per_page=100')
+                .then((response) => {
+                    return response.json();
+                })
+                .then((res) => {
+                    return {
+                        query: query,
+                        images: this.reformatDb(res.hits)
+                    }
+                });
+        }
+
+        search(query, searchModuleId = 'local') {
+            if (this.query[searchModuleId][query]) {
+                return Promise.resolve(this.query[searchModuleId][query]);
+            }
+
+            return this[searchModuleId + 'Search'](query)
+                .then((res) => {
+                    this.query[searchModuleId][query] = res;
+                    return this.query[searchModuleId][query];
+                });
+        }
     }
 
-    search(query) {
-      return {
-        query: 'example',
-        images: [
-          {
-            id: 1,
-            url: '/img/mammal-3162194_640.jpg',
-            tags: 'panda'
-          },
-          {
-            id: 2,
-            url: '/img/panda-659186_640.png',
-            tags: 'panda'
-          }
-        ]
-      };
-    }
-  }
-
-  return ImagesResolver;
+    return ImagesResolver;
 })();
